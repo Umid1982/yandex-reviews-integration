f0ee469d9f9783bfb4073990d7be9e5174887963
Umid1982 <111682944+Umid1982@users.noreply.github.com>
2025-11-19 03:04:50 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-
 export default defineConfig({
+    base: '',   // <--- добавляем ЭТО
+
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
@@ -14,7 +12,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-
-        vue(), // ← без лишних настроек, плагин сам всё сделает
+        vue(),
+        tailwindcss(),
     ],
 });
